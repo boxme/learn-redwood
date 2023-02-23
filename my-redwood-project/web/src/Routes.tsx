7098316796984a18aa91a99970666cf6be33f325
Desmond <desmond.ng.yang.yi@gmail.com>
2023-02-23 21:50:33 +0800

    Create contact page

--- my-redwood-project/web/src/Routes.tsx
+++ my-redwood-project/web/src/Routes.tsx
@@ -14,7 +14,7 @@ import BlogLayout from './layouts/BlogLayout/BlogLayout'
 
 const Routes = () => {
   return (
-    <Router>      
+    <Router>                  
       <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
         <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
         <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
@@ -23,6 +23,7 @@ const Routes = () => {
       </Set>
       <Set wrap={BlogLayout}>
         <Route path='/article/{id:Int}' page={ArticlePage} name='article' />
+        <Route path="/contact" page={ContactPage} name="contact" />
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
       </Set>
