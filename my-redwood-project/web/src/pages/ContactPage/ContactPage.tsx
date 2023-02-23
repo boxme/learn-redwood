e27e2fc61da3d8b315d150eb146d2f33ca199eea
Desmond <desmond.ng.yang.yi@gmail.com>
2023-02-23 22:47:18 +0800

    graphql and service logic for contact form input

--- my-redwood-project/web/src/pages/ContactPage/ContactPage.tsx
+++ my-redwood-project/web/src/pages/ContactPage/ContactPage.tsx
@@ -1,10 +1,11 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags, useMutation } from '@redwoodjs/web'
-import { FieldError, Form, Label, Submit, SubmitHandler, TextField } from '@redwoodjs/forms'
+import { FieldError, Form, FormError, Label, Submit, SubmitHandler, TextField, useForm } from '@redwoodjs/forms'
 import {
   CreateContactMutation,
   CreateContactMutationVariables,
 } from 'types/graphql'
+import { toast, Toaster } from '@redwoodjs/web/dist/toast'
 
 const CREATE_CONTACT = gql`
 mutation CreateContactMutation($input: CreateContactInput!) {
@@ -21,10 +22,16 @@ interface FormValues {
 }
 
 const ContactPage = () => {
-  const [create] = useMutation<
+  const formMethods = useForm({ mode: 'onBlur' })
+  const [create, {loading, error}] = useMutation<
     CreateContactMutation,
     CreateContactMutationVariables
-  >(CREATE_CONTACT)
+  >(CREATE_CONTACT, {
+    onCompleted: () => {
+      toast.success("Thank you for your submission!")
+      formMethods.reset()
+    }
+  })
 
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     console.log(data)
@@ -34,7 +41,9 @@ const ContactPage = () => {
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+      <Toaster />
+      <Form onSubmit={onSubmit} error={error} formMethods={formMethods}>
+        <FormError error={error} wrapperClassName="form-error"/>
         <Label name="name" errorClassName="error">Name</Label>
         <TextField name="name" validation={{required: true}} errorClassName="error"/>
         <FieldError name="name" className="error"/>
@@ -57,7 +66,7 @@ const ContactPage = () => {
         <TextField name="message" validation={{required: true}} errorClassName="error"/>
         <FieldError name="message" className="error"/>
 
-        <Submit>Save</Submit>
+        <Submit disabled={loading}>Save</Submit>
       </Form>
     </>
   )
