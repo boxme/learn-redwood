57018210a0f83ba95dbdf30abfe93e083a381694
Desmond <desmond.ng.yang.yi@gmail.com>
2023-02-24 16:35:07 +0800

    Added authentication model and secure admin posts with private set

--- my-redwood-project/web/src/Routes.tsx
+++ my-redwood-project/web/src/Routes.tsx
@@ -7,20 +7,24 @@
 // 'src/pages/HomePage/HomePage.js'         -> HomePage
 // 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
 
-import { Router, Route, Set } from '@redwoodjs/router'
+import { Router, Route, Set, Private } from '@redwoodjs/router'
 
 import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 import BlogLayout from './layouts/BlogLayout/BlogLayout'
 
+import { useAuth } from './auth'
+
 const Routes = () => {
   return (
-    <Router>                  
-      <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
-        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
-        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
-        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
-        <Route path="/posts" page={PostPostsPage} name="posts" />
-      </Set>
+    <Router useAuth={useAuth}>    
+      <Private unauthenticated="home">
+        <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
+          <Route path="/admin/posts/new" page={PostNewPostPage} name="newPost" />
+          <Route path="/admin/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+          <Route path="/admin/posts/{id:Int}" page={PostPostPage} name="post" />
+          <Route path="/admin/posts" page={PostPostsPage} name="posts" />
+        </Set>
+      </Private>              
       <Set wrap={BlogLayout}>
         <Route path='/article/{id:Int}' page={ArticlePage} name='article' />
         <Route path="/contact" page={ContactPage} name="contact" />
