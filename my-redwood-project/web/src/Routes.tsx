5ea5d010dae27f958914ad605b29773cb1f5b40a
Desmond <desmond.ng.yang.yi@gmail.com>
2023-02-24 16:52:18 +0800

    Authentication functionality

--- my-redwood-project/web/src/Routes.tsx
+++ my-redwood-project/web/src/Routes.tsx
@@ -13,10 +13,18 @@ import ScaffoldLayout from 'src/layouts/ScaffoldLayout'
 import BlogLayout from './layouts/BlogLayout/BlogLayout'
 
 import { useAuth } from './auth'
+import SignupPage from './pages/SignupPage/SignupPage'
+import LoginPage from './pages/LoginPage/LoginPage'
+import ForgotPasswordPage from './pages/ForgotPasswordPage/ForgotPasswordPage'
+import ResetPasswordPage from './pages/ResetPasswordPage/ResetPasswordPage'
 
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>    
+      <Route path="/login" page={LoginPage} name="login" />    
+      <Route path="/signup" page={SignupPage} name="signup" />    
+      <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />    
+      <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />    
       <Private unauthenticated="home">
         <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
           <Route path="/admin/posts/new" page={PostNewPostPage} name="newPost" />
